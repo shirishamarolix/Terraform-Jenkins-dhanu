8f26f5c4d522493328418307d0ee2a5553e06a2a
rayenkidhanunjaya <119997642+rayenkidhanunjaya@users.noreply.github.com>
2023-08-22 13:18:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,14 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0453898e98046c639" # us-east-1
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = true
+    enable_dns_hostnames = true
   tags = {
-      Name = "dhanu-instance"
+    Name = "dhanu01"
+    Environment = "DEV"
   }
 }
+  
